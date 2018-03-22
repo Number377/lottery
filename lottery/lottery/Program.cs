b0b5eb1272c03f741fc1b97e39ce4e4c594a3088
m305-33\w401 <a106082030@mail.shu.edu.tw>
2018-03-22 18:01:38 +0800

    亂數增加為6個

--- lottery/lottery/Program.cs
+++ lottery/lottery/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace lottery
 {
@@ -10,10 +11,48 @@ namespace lottery
     {
         static void Main(string[] args)
         {
-            Random rand = new Random();
-            Console.WriteLine(rand.Next(0, 100));
+            // 自體顏色與背景顏色
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.BackgroundColor = ConsoleColor.Gray;
 
-            Console.ReadLine();
+            // 生成亂數物件
+            Random rand = new Random();
+
+            // 次數
+            int count = 1;
+            
+
+            // 重複產生
+            while (count <= 10)
+            {
+                Console.Clear();
+
+                // 產生六個亂數
+                int r1 = rand.Next(1, 43);
+                int r2 = rand.Next(1, 43);
+                int r3 = rand.Next(1, 43);
+                int r4 = rand.Next(1, 43);
+                int r5 = rand.Next(1, 43);
+                int r6 = rand.Next(1, 43);
+
+                // 上外框
+                Console.WriteLine("╒═╤═╤═╤═╤═╤═╕");
+
+                // 顯示亂數
+                Console.WriteLine("{0:00}  {1:00}  {2:00}  {3:00}  {4:00}  {5:00}  {6:00}",count, r1, r2, r3, r4, r5, r6);
+
+                // 下外框
+                Console.WriteLine("╘═╧═╧═╧═╧═╧═╛");
+
+                // 次數+1
+                count++;
+
+                //延遲0.1秒
+                Thread.Sleep(100);
+            }
+            
+            // 暫停畫面
+            Console.ReadKey();
         }
     }
 }
